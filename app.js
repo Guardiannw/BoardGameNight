b72a18e95927840794e2614fe518ae1cdcf9071b
Nathaniel Rankin Webb <nrwebb@live.com>
2018-05-28 10:06:57 -0500

    Refactor.

--- app.js
+++ app.js
@@ -1,82 +1,62 @@
 //Meetup API autorization for OAuth
-const loginURL = `https://secure.meetup.com/oauth2/authorize?client_id=flaq16ghlsndfol2m7jkfe1pfk&response_type=token&redirect_uri=https://jmkeller3.github.io/BoardGameNight/`;
+const loginURL = `https://secure.meetup.com/oauth2/authorize?client_id=4m8k9ur03980t5eh8hnl0g8363&response_type=token&redirect_uri=https://guardiannw.github.io/BoardGameNight/`;
 const url = window.location.href;
 const regex = /(?:#|\?|&)(?:([a-zA-Z_]+)=([^&]+))*/g;
 let matcher;
 let queryParams = {};
 //defining crucial varibles for map and Meetup search url
-let user_lat; 
-let user_lon;
-let initialLocation;
 while(matcher = regex.exec(url)) {
     const [,key, val] = matcher;
     queryParams[key] = val;
 }
 
-
-
 //google map api
 let addMarker;
-var map;
-var options;
 function initMap() {
-    var mapOptions = {
-        zoom: 9,
-        center: initialLocation,
-    };
-    var map = new google.maps.Map(document.getElementById('map'),
-    mapOptions);
+    if (!Object.keys(queryParams).length)
+        window.location.href = loginURL;
 
-    addMarker = initAddMarkerWithMap(map);
-
-    findGeo();
-}
-
-//accesses user's location and sets the location to the global varibles
-function findGeo() {
-    console.log(`findGeo is working`);
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            console.log(position.coords.latitude, position.coords.longitude);
-            user_lat = position.coords.latitude;
-            user_lon = position.coords.longitude;
-            initialLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
-            console.log(`user_lat is ${user_lat} and is working`);
-            console.log(`user_lon is ${user_lon} and is working`);
+            let user_lat = position.coords.latitude;
+            let user_lon = position.coords.longitude;
+            let initialLocation = { lat: user_lat, lng: user_lon };
+
+            let mapOptions = {
+                zoom: 9,
+                center: initialLocation,
+            };
+        
+            let map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+
+            addMarker = initAddMarkerWithMap(map);
+
             //resets the map to be centered on the user's location
-            initMap();
-            MeetupLogin();
+            fetchMeetupData(user_lat, user_lon);
         });
     };
-    }
+}
+
 //takes user to the login page to allow access from Meetup
 //reloads page with JSONP data with nearby events
-function MeetupLogin() {
-    console.log(`MeetupLogin is working`);
-    if (Object.keys(queryParams).length) {
-        // Authenticated
-        console.log(`${user_lon} is and is working`);
-        const requestURL = `https://api.meetup.com/2/concierge?access_token=${queryParams.access_token}&lon=${user_lon}&category_id=11&radius=smart&lat=${user_lat}`;
-        console.log(queryParams);
-        $.ajax(requestURL, {
-            dataType: 'jsonp',
-            success: function (data) {
-                console.log(data);
-                displayresults(data);
-            }
-        });
-    }
-    else
-        window.location.href = loginURL;
+function fetchMeetupData(lat, lon) {
+    const requestURL = `https://api.meetup.com/2/concierge?access_token=${queryParams.access_token}&lon=${lon}&category_id=11&radius=smart&lat=${lat}`;
+
+    $.ajax(requestURL, {
+        dataType: 'jsonp',
+        success: (data) => {
+            displayresults(data);
+        }
+    });
 }
 
 //adds customer marker to map
 function initAddMarkerWithMap(map) {
-    console.log(`made a marker`);
     return function addMarker(coords) {
-            var marker = new google.maps.Marker({
+            let marker = new google.maps.Marker({
                 position: coords,
-                map: map,
+                map,
                 icon: {
                     url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/156px-Map_marker.svg.png',
                     scaledSize: new google.maps.Size(18,24)
@@ -87,10 +67,9 @@ function initAddMarkerWithMap(map) {
 //takes a result and returns the time, place, description, and name 
 //the event
 function renderResults(result) {
-    console.log(`ran renderResults just fine`);
     let time = new Date(result.time);
     let date = time.toString('MMM dd');
-    
+
     const venueExists = result.venue !== undefined;
     const groupExists = result.group !== undefined;
 
@@ -104,13 +83,11 @@ function renderResults(result) {
         longitude = result.group.group_lon;
     }
     //displays a marker on map for event
-    function renderLatAndLon () {
-        if (venueExists || groupExists) {
-            const pin = {lat: latitude, lng: longitude}
-            addMarker(pin);                
-        } else {
-            console.log(`No latitude and longitude availible.`);
-        }
+    if (venueExists || groupExists) {
+        const pin = {lat: latitude, lng: longitude}
+        addMarker(pin);
+    } else {
+        console.log(`No latitude and longitude availible.`);
     }
     
     return (`
@@ -127,5 +104,4 @@ function renderResults(result) {
 function displayresults(data) {
     const events = data.results.map((item, index) => renderResults(item));
     $('.js-results').html(events);
-    console.log(`displayresults ran`);
 }
